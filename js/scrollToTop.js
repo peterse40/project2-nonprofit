333954e233a7707d82f4fd174ddc49701fe61f2d
peterse40 <elizabethpeters@Elizabeths-MacBook-Pro.local>
2016-11-28 16:43:33 -0600

    worked on html
    scroll to top

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,19 +1,35 @@
 // JavaScript Document
-    $(document).ready(function(){ 
- 
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 100) {
-                $('.scrollup').fadeIn();
-            } else {
-                $('.scrollup').fadeOut();
-            }
-        }); 
- 
-        $('.scrollup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 550);
-            return false;
-        });
- 
-    });
-
+var scrollY = 0;
+var distance = 40;
+var speed = 24;
+function autoScrollTo(el) {
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementbyId(el).offsetTop;
+    var bodyHeight = document.body.offsetHeight;
+    var yPos = currentY + window.innerHeight;
+    var animator = setTimeout('autoScrollTo(\''+el+'\')', 24);
+    if(yPos > bodyHeight){
+        clearTimeout(animator);
     
+    } else {
+        if(currentY < targetY-distace){
+            scrollY = currentY+distance;
+            window.scroll(0, scrollY);
+
+        } else {
+            clearTimeout(animator);
+        }
+    }
+}
+function resetScroller(el) {
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementbyId(el).offsetTop;
+    var animator = setTimeout('resetScroller(\''+el+'\')', speed);
+    if(currentY > targetY) {
+        scrollY = currentY-distance;
+        window.scroll(0, scrollY);
+
+    } else {
+        clearTimeout(animator);
+    }
+}
